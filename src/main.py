34c4d7224d63dc67ae759f901fedb3d5d8767e59
Vladislav <barabayne@gmail.com>
2024-12-05 02:21:58 +0500

    fix: delete check_ip route

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 from contextlib import asynccontextmanager
 
-from fastapi import Depends, FastAPI, Request
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import FastAPI
 
-from src.database.database import get_async_session
 from src.routers.board_router import board_router
 from src.routers.post_router import post_router
 from src.routers.thread_router import thread_router
 from src.routers.user_router import user_router
-from src.services.user_service import UserService, get_user_service
 
 
 @asynccontextmanager
@@ -17,16 +14,6 @@ async def lifespan(_):
 
 
 app = FastAPI(title='1 Chan', lifespan=lifespan)
-
-
-@app.get('/check_ip')
-async def check_ip(
-    request: Request,
-    user_service: UserService = Depends(get_user_service),
-    session: AsyncSession = Depends(get_async_session),
-):
-    user = await user_service.check_ip(ip_address=request.client.host, session=session)
-    return user
 
 
 app.include_router(board_router._router)
